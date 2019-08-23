9ca77f416c52abe3a0423cc402f6977f0ffe4945
Daniel Ontiveros <cardanont@gmail.com>
2019-08-23 18:09:34 -0500

    Implementation of an array with push and pop

---
+++ Arrays/ImplementingAnArray.js
@@ -0,0 +1,23 @@
+class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index]
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        const lasItem = this.data[this.length -1]
+        delete this.data[this.length - 1]
+        this.length--;
+        return lasItem;
+    }
+}
