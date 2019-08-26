26ac77a2f82ab39e83cc5e97d48a1ccda4c66267
Daniel Ontiveros <cardanont@gmail.com>
2019-08-26 01:13:15 -0500

    check for the initial input

--- Arrays/ReverseString.js
+++ Arrays/ReverseString.js
@@ -1,9 +1,12 @@
 // [Hello My Name is Cardanont]
 
-last = "Hello My name is";
-
 function reverse(str) {
     
+    // Check input
+    if(!str || str.length < 2 || typeof str !== "string"){
+        return 'Something is not ok with the imput string'
+    }
+
     reversed = [];
 
     for(let i = str.length -1; i >= 0; i--){
