b6136fc881b97d6742cc521b43b9fe2648f14f75
Daniel Ontiveros <cardanont@gmail.com>
2019-08-28 23:59:55 -0500

    the collition :/

--- HashTables/HashCollitions.js
+++ HashTables/HashCollitions.js
@@ -10,3 +10,6 @@ let user = {
 user.age // O(1)
 user.spell = 'abra kadabra'; // O(1)
 user.scream(); // O(1)
+
+//Collision stores more data on the same memory space this due to the limited amount of memory and it slows de input and output.
+// O(n/k) -> O(n)
