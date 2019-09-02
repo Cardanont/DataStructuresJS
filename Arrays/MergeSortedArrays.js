e2ddf604390d516a3d0cb1a34b24a5dfc027fb7d
Daniel Ontiveros <cardanont@gmail.com>
2019-09-02 15:16:48 -0500

    Experimenting with arrays

--- Arrays/MergeSortedArrays.js
+++ Arrays/MergeSortedArrays.js
@@ -28,5 +28,35 @@ function mergeSortedArrays(arr1, arr2){
     }
     return merged;
 }
-
 mergeSortedArrays([0,3,4,31],[4,6,30]);
+
+function mergeTheFish(arr1, arr2) {
+	let arr1Item = arr1[0];
+	let arr2Item = arr2[0];
+    const result = [];
+	let i = 1;
+    let j = 1;    
+    
+    // Check empty arrays 
+    if(arr1 === 0){
+        return arr2;
+    }
+    if(arr2 === 0){
+        return arr1;
+    }
+
+    while(arr1Item & arr2Item) {
+        if(!arr2Item || arr1Item < arr2Item) {
+        result.push(arr1Item);
+        arr1Item = arr1[i];
+        i++;
+        }else{
+            result.push(arr2Item);
+            arr2Item = arr2[j];
+            j++;    
+        }
+    }
+    return result;
+}
+
+mergeTheFish(['Cod','Herring','Marling'],['Asp','Carp','Ide','Trout']);
